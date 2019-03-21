e81718ebbc9acb81b0c12a76e4e2f84666423c7f
Chris Baucom <crbaucom@gmail.com>
2019-03-20 22:07:45 -0400

    Add data endpoint for frontend to hit

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ import "./lib/cron";
 
 const app = express();
 
-app.get("/scrape", async (req, res, next) => {
+app.get(`/scrape`, async (req, res, next) => {
   console.log("Scraping...!");
   const [instaCount, twitterCount] = await Promise.all([
     getInstagramCount(),
@@ -15,6 +15,13 @@ app.get("/scrape", async (req, res, next) => {
   res.json({ instaCount, twitterCount });
 });
 
+app.get(`/data`, async (req, res, next) => {
+  // get the scrape data
+  const { instagram, twitter } = db.value();
+
+  res.json({ instagram, twitter });
+});
+
 app.listen(8444, () => {
-  console.log("Scrape app listening on port 8444");
+  console.log(`Scrape app listening on http://localhost:8444`);
 });
