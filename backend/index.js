2b5d45b70ecd28e743008ddb1fa05be976f390f2
Chris Baucom <crbaucom@gmail.com>
2019-03-20 22:08:23 -0400

    Add cors

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import { getTwitterCount, getInstagramCount } from "./lib/scraper";
 import db from "./lib/db";
 import "./lib/cron";
 
 const app = express();
+app.use(cors());
 
 app.get(`/scrape`, async (req, res, next) => {
   console.log("Scraping...!");
